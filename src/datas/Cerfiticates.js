3e60510acafda3f65385b130857e12489e223ac0
ford456 <fordllawat@gmail.com>
2025-09-02 17:03:15 +0700

    up

--- src/datas/Cerfiticates.js
+++ src/datas/Cerfiticates.js
@@ -23,4 +23,24 @@ export const Cerfiticates = [
 
 
     },
+    {
+        id: '02',
+        title: "Generative AI และการใช้งาน ChatGPT เพื่อใช้ในการศึกษาค้นคว้า",
+        Content: (
+            <div>
+                <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8 indent-8">
+                    รายวิชานี้มุ่งให้ผู้เรียนเข้าใจพื้นฐาน Generative AI และการใช้ ChatGPT อย่างเหมาะสมในงานวิชาการ พร้อมฝึกทักษะ prompt และวิเคราะห์ผลอย่างมีวิจารณญาณ
+                </p>
+                <img
+                    src="/Cer/Cer02.JPG"
+                    alt="Certificate of Generative AI"
+                    width={500}
+                    height={500}
+                    className="rounded-lg object-contain md:object-cover h-30 md:h-44 lg:h-85 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+            </div>
+        ),
+
+
+    },
 ] 
