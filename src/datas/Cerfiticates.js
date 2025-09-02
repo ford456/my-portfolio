682cd1057448eafc0ee97c767dad6deb6ee37470
ford456 <fordllawat@gmail.com>
2025-09-02 18:36:46 +0700

    update 02.09.25

--- src/datas/Cerfiticates.js
+++ src/datas/Cerfiticates.js
@@ -7,7 +7,7 @@ export const Cerfiticates = [
         id: '01',
         title: "Certificate of CompletionComplete Web & Mobile Designer",
         Content: (
-            <div>
+            <div >
                 <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
                     Complete Web & Mobile Designer: UX/UI, Figma, +more
                 </p>
@@ -16,7 +16,7 @@ export const Cerfiticates = [
                     alt="Certificate of Completion"
                     width={500}
                     height={500}
-                    className="rounded-lg object-contain md:object-cover h-30 md:h-44 lg:h-85 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                    className="rounded-lg object-contain md:object-cover h-30 md:h-44 lg:h-75 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                 />
             </div>
         ),
@@ -27,7 +27,7 @@ export const Cerfiticates = [
         id: '02',
         title: "Generative AI และการใช้งาน ChatGPT เพื่อใช้ในการศึกษาค้นคว้า",
         Content: (
-            <div>
+            <div  className="flex flex-col items-center">
                 <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8 indent-8">
                     รายวิชานี้มุ่งให้ผู้เรียนเข้าใจพื้นฐาน Generative AI และการใช้ ChatGPT อย่างเหมาะสมในงานวิชาการ พร้อมฝึกทักษะ prompt และวิเคราะห์ผลอย่างมีวิจารณญาณ
                 </p>
@@ -36,7 +36,7 @@ export const Cerfiticates = [
                     alt="Certificate of Generative AI"
                     width={500}
                     height={500}
-                    className="rounded-lg object-contain md:object-cover h-30 md:h-44 lg:h-85 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                    className="rounded-lg object-contain md:object-cover h-30 md:h-44 lg:h-75 w-full max-w-md shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                 />
             </div>
         ),
@@ -47,7 +47,7 @@ export const Cerfiticates = [
         id: '03',
         title: "Graphic Design Masterclass - Learn GREAT Design",
         Content: (
-            <div>
+            <div className="flex flex-col items-center">
                 <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8 indent-8">
                     The Ultimate Graphic Design Course Which Covers Photoshop, Illustrator, InDesign, Design Theory, Branding, Logo Design
                 </p>
@@ -56,7 +56,7 @@ export const Cerfiticates = [
                     alt="Graphic Design Masterclass"
                     width={500}
                     height={500}
-                    className="rounded-lg object-contain md:object-cover h-30 md:h-44 lg:h-85 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                    className=" rounded-lg object-contain object-center md:object-cover h-30 md:h-44 lg:h-75 w-full max-w-md shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                 />
             </div>
         ),
