d6b6e7be62948706b587db49a4f62c241169bb2a
ford456 <fordllawat@gmail.com>
2025-09-02 23:32:31 +0700

    up

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -7,9 +7,9 @@ export default function NotFound() {
 
 
   return (
-    <div className='container bg-[#131313] h-[300px] lg:h-[600px] w-fill mx-auto px-3 sm:px-3 lg:px-10 pt-20 lg:pt-5'>
-      <div className='pt-[15%] flex flex-col items-center justify-center'>
-        <h2 className='text-6xl p-4 font-bold'>404 Not Found</h2>
+    <div className='flex items-center justify-center h-dvh max-h-1/2 mx-auto'>
+      <div className=' flex flex-col items-center justify-center'>
+        <h2 className='text-4xl lg:text-6xl p-4 font-bold'>404 Not Found</h2>
         <p>Could not find requested resource</p>
         <p className='pt-5'>
           <Link href="/" className="font-bold text-blue-200 hover:text-blue-500" target='_parent' >Back to Home</Link>
