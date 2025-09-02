6e237d834ee2642b23a5e0c23c23126e2a8b13cc
ford456 <fordllawat@gmail.com>
2025-09-02 18:50:57 +0700

    update cer04

--- src/datas/Cerfiticates.js
+++ src/datas/Cerfiticates.js
@@ -63,4 +63,24 @@ export const Cerfiticates = [
 
 
     },
+    {
+        id: '04',
+        title: "UI/UX Design With Figma : 5+ Real World Projects",
+        Content: (
+            <div className="flex flex-col items-center">
+                <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8 indent-8">
+                    Become a Designer in 2025! Learn how to use Figma to design beautiful mobile & web apps Learn-by-doing approach.
+                </p>
+                <img
+                    src="/Cer/Cer04.jpg"
+                    alt="Graphic Design Masterclass"
+                    width={500}
+                    height={500}
+                    className=" rounded-lg object-contain object-center md:object-cover h-30 md:h-44 lg:h-75 w-full max-w-md shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                />
+            </div>
+        ),
+
+
+    },
 ] 
