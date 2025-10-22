e46d242ce9ac3b1ac314a01b80e3b3cb60cf5dda
ford456 <fordllawat@gmail.com>
2025-10-23 00:08:26 +0700

    update

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -83,7 +83,7 @@ export const Timeline = ({
                         >
                             <div className="relative pl-20 pr-4 md:pl-4 w-full">
                                 <h3
-                                    className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
+                                    className="md:hidden block text-2xl mb-4 text-left font-bold text-blue-500">
                                     {item.title}
                                 </h3>
                                 {item.content}{" "}
